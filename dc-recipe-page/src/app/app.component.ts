08593eeda345adcdbc77822ee9e1f41edbd8363b
Manuel Brás <manuel.madeira.bras@gmail.com>
2023-01-30 18:22:39 +0000

    included instructions section

--- dc-recipe-page/src/app/app.component.ts
+++ dc-recipe-page/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dc-recipe-page';
+export class AppComponent implements AfterViewInit {
+	@ViewChildren(MatCheckbox) checkboxes!: QueryList<MatCheckbox>;
+
+	ngAfterViewInit(): void {
+		this.checkboxes.forEach((checkbox, index) => {
+			const cachedCheckboxValue = localStorage.getItem(`checkbox-${index}`);
+
+			if (cachedCheckboxValue === '1') {
+				checkbox.checked = true;
+            } else {
+				checkbox.checked = false;
+            }
+		});
+	}
+
+	updateOnCheck(checkbox: MatCheckbox): void {
+		const index = this.checkboxes.toArray().indexOf(checkbox);
+
+		if (!checkbox.checked) {
+			localStorage.removeItem(`checkbox-${index}`);
+		} else {
+			localStorage.setItem(`checkbox-${index}`, '1');
+		}
+	}
 }
